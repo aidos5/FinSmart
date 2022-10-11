eb73abf9b22be484ac92d47bd37509bfb1ae17e6
Aidos532003 <aidos532003@gmail.com>
2022-10-11 19:25:40 +0530

    Search field

--- hecker/lib/Items.dart
+++ hecker/lib/Items.dart
@@ -1,127 +1,164 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:hecker/AddItems.dart';
 import 'package:hecker/Model/ModelItem.dart';
-import 'package:hecker/Navigation.dart';
 
 class Items extends StatefulWidget {
-  Items({Key? key}) : super(key: key);
+  const Items({Key? key}) : super(key: key);
 
   @override
   State<Items> createState() => _ItemsState();
 }
 
 class _ItemsState extends State<Items> {
+  Stream<List<ModelItem>> dataItems = readItem();
+
+  List<ModelItem> allItems = [];
+  List<ModelItem> foundItems = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    dataItems.listen((listofitems) {
+      for (ModelItem i in listofitems) {
+        allItems.add(i);
+      }
+    });
+    super.initState();
+  }
+
+  final controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final screenwidth = MediaQuery.of(context).size.width;
-    return WillPopScope(
-      onWillPop: () async {
-        print('Back button pressed');
-        return false;
-      },
-      child: Scaffold(
-          resizeToAvoidBottomInset: true,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => AddItems()),
-                  (route) => false);
-            },
-            child: Icon(Icons.add),
-          ),
-          appBar: AppBar(
-            centerTitle: true,
-            
-            title: Text(
-              'FinSmart',
-              style: TextStyle(fontSize: 37),
-              
-            ),
-          ),
-          body: StreamBuilder<List<ModelItem>>(
-            stream: readItem(),
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'FinSmart',
+          style: TextStyle(fontSize: 37),
+        ),
+      ),
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: SizedBox(
+          width: screenwidth,
+          child: StreamBuilder<List<ModelItem>>(
+            stream: dataItems,
             builder: (context, snapshot) {
               if (snapshot.hasError) {
                 return Text('There is an error ${snapshot.error}');
               } else if (snapshot.hasData) {
-                final items = snapshot.data!;
-
-                return ListView.builder(
-                  // itemBuilder: (context, index) {
-                  //   items.map(buildItem).toList();
-                  //   throw '';
-                  // },
-
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    final sortedItems = items.reversed;
-                    return Card(
-                      child: Column(
-                        children: [
-                          // ignore: unnecessary_string_interpolations
-                          Row(
-                            children: [
-                              Text(
-                                'Name : ',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              Text(
-                                '${items[index].name}',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              SizedBox(
-                                width: 25,
-                              ),
-                              Text(
-                                'Quantity left: ',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              Text(
-                                '${items[index].quantity} ',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              Text('${items[index].unit}',
-                                  style: TextStyle(fontSize: 20)),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                'Selling Price : ',
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              Text('${items[index].price}',
-                                  style: TextStyle(fontSize: 20)),
-                            ],
-                          ),
-                          MaterialButton(
-                            onPressed: (() {}),
-                            child: Text('Update'),
-                            color: Colors.green,
-                          )
-                        ],
+                foundItems = List.from(allItems);
+                return Column(
+                  children: [
+                    TextField(
+                      controller: controller,
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.search),
+                        hintText: 'Search Item',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.blueAccent),
+                        ),
                       ),
-                    );
-                  },
+                      onChanged: searchItems,
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: foundItems.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Column(
+                            children: [
+                              // ignore: unnecessary_string_interpolations
+                              Row(
+                                children: [
+                                  Text(
+                                    'Name : ',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Text(
+                                    '${foundItems[index].name}',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  SizedBox(
+                                    width: 25,
+                                  ),
+                                  Text(
+                                    'Quantity left: ',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Text(
+                                    '${foundItems[index].quantity} ',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Text('${foundItems[index].unit}',
+                                      style: TextStyle(fontSize: 20)),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'Selling Price : ',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  Text('${foundItems[index].price}',
+                                      style: TextStyle(fontSize: 20)),
+                                ],
+                              ),
+                              MaterialButton(
+                                onPressed: (() {}),
+                                child: Text('Update'),
+                                color: Colors.green,
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ],
                 );
               } else {
-                return Center(
+                return const Center(
                   child: CircularProgressIndicator(),
                 );
               }
             },
-          )),
+          ),
+        ),
+      ),
     );
   }
 
-  Stream<List<ModelItem>> readItem() => FirebaseFirestore.instance
-      .collection('transactions')
-      .doc('category')
-      .collection('pincode')
-      .doc('shopid')
-      .collection('items')
-      .snapshots()
-      .map((snapshot) =>
-          snapshot.docs.map((doc) => ModelItem.fromJson(doc.data())).toList());
+  void searchItems(String query) {
+    List<ModelItem> results = [];
+    if (query.isEmpty) {
+      results.addAll(allItems);
+    } else {
+      results = foundItems.where((item) {
+        final itemName = item.name.toLowerCase();
+        query = query.toLowerCase();
+        return itemName.contains(query);
+      }).toList();
+    }
+
+    setState(() {
+      foundItems = List.from(results);
+    });
+  }
+
+  static Stream<List<ModelItem>> readItem() {
+    return FirebaseFirestore.instance
+        .collection('transactions')
+        .doc('category')
+        .collection('pincode')
+        .doc('shopid')
+        .collection('items')
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => ModelItem.fromJson(doc.data()))
+            .toList());
+  }
 }
